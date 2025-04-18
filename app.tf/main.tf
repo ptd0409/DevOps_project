805468c008f21cd39458c840b8ab7fa202f8940c
ptd0409 <116495035+ptd0409@users.noreply.github.com>
2025-04-18 12:33:37 +0700

    Update main.tf

--- app.tf/main.tf
+++ app.tf/main.tf
@@ -26,7 +26,7 @@ module "eks" {
   cluster_version = "1.28"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
-  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true
   cluster_endpoint_public_access_cidrs = ["171.252.153.71/32"]
 
   eks_managed_node_groups = {
