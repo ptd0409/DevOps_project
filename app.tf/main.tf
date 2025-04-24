173c8d77d9fa274ee4c746ad488da979bf05d8f1
Dung Pham <leonardcurryst@gmail.com>
2025-04-24 14:17:31 +0000

    :sparkles: merge all things

--- app.tf/main.tf
+++ app.tf/main.tf
@@ -26,6 +26,8 @@ module "eks" {
   cluster_version = "1.28"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
+  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   eks_managed_node_groups = {
     dev = {
